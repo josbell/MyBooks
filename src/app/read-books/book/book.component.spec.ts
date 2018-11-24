1cd7d7c45f8f96623924e783ab75fb06c1d650ea
Josbell Quiros <jquiros@symplicity.com>
2018-11-24 17:22:40 -0500

    Add Book Component Test

--- src/app/read-books/book/book.component.spec.ts
+++ src/app/read-books/book/book.component.spec.ts
@@ -1,84 +1,85 @@
-// import { SharedModule } from './../../shared/shared.module';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { NO_ERRORS_SCHEMA, Input } from '@angular/core';
+import { SharedModule } from './../../shared/shared.module';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA, Input, Directive } from '@angular/core';
 
-// import { BookComponent } from './book.component';
-// import { By } from '@angular/platform-browser';
-// import { Book } from '../../core/book';
+import { BookComponent } from './book.component';
+import { By } from '@angular/platform-browser';
+import { Book } from '../../core/book';
+import { AppMaterialModule } from '../../shared/app-material.module';
 
-// @Directive({
-//   selector: '[routerLink]',
-//   host: { '(click)': 'onClick()' }
-// })
-// export class RouterLinkDirectiveStub {
-//   @Input('routerLink') linkParams: string;
-//   navigatedTo: any = null;
+@Directive({
+  selector: '[routerLink]',
+  host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: string;
+  navigatedTo: any = null;
 
-//   onClick() {
-//     this.navigatedTo = this.linkParams;
-//   }
-// }
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
 
-// describe('BookComponent', () => {
-//   let component: BookComponent;
-//   let fixture: ComponentFixture<BookComponent>;
+describe('BookComponent', () => {
+  let component: BookComponent;
+  let fixture: ComponentFixture<BookComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [SharedModule, RouterLinkDirectiveStub],
-//       declarations: [ BookComponent ],
-//       schemas: [NO_ERRORS_SCHEMA],
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppMaterialModule],
+      declarations: [BookComponent, RouterLinkDirectiveStub ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(BookComponent);
-//     component = fixture.componentInstance;
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookComponent);
+    component = fixture.componentInstance;
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should have the correct book', () => {
-//     component.book = new Book({id: '1', title: 'Book Name Test'});
+  it('should have the correct book', () => {
+    component.book = new Book({id: '1', title: 'Book Name Test'});
 
-//     expect(component.book.title).toEqual('Book Name Test');
-//   });
+    expect(component.book.title).toEqual('Book Name Test');
+  });
 
-//   it('should render the book title', () => {
-//     component.book = new Book({id: '1', title: 'Book Name Test'});
-//     fixture.detectChanges();
+  it('should render the book title', () => {
+    component.book = new Book({id: '1', title: 'Book Name Test'});
+    fixture.detectChanges();
 
-//     expect(fixture.nativeElement.textContent).toContain('Book Name Test');
-//   });
+    expect(fixture.nativeElement.textContent).toContain('Book Name Test');
+  });
 
-//   it('should call onDeleteClick method when delete button clicked', () => {
-//     spyOn(component, 'onDeleteClick');
-//     fixture.debugElement.query(By.css('button.delete')).triggerEventHandler('click', {});
+  it('should call onDeleteClick method when delete button clicked', () => {
+    component.book = new Book({ id: '1', title: 'Book Name Test' });
+    spyOn(component, 'onDeleteClick');
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('button')).triggerEventHandler('click', {});
 
-//     expect(component.onDeleteClick).toHaveBeenCalled();
-//   });
+    expect(component.onDeleteClick).toHaveBeenCalled();
+  });
 
-//   it('should emit delete event when onDeleteClick is called', () => {
-//     spyOn(component.delete, 'emit');
-//     component.onDeleteClick();
+  it('should emit delete event when onDeleteClick is called', () => {
+    spyOn(component.delete, 'emit');
+    component.onDeleteClick();
 
-//     expect(component.delete.emit).toHaveBeenCalledWith('');
-//   });
+    expect(component.delete.emit).toHaveBeenCalledWith('');
+  });
 
-//   it('should have the correct route', () => {
-//     fixture.detectChanges();
-//     const bookComponents = fixture.debugElement.queryAll(By.directive(BookComponent));
+  it('should navigate to correct route when clicked', () => {
+    component.book = new Book({ id: '1', title: 'Book Name Test' });
+    fixture.detectChanges();
 
-//     // Get mock router from first book component
-//     const routerLink = bookComponents[0]
-//       .query(By.directive(RouterLinkDirectiveStub))
-//       .injector.get(RouterLinkDirectiveStub);
+    const routerLink = fixture.debugElement
+      .query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
 
-//     bookComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('a')).triggerEventHandler('click', null);
 
-//     expect(routerLink.navigatedTo).toBe('/detail/1');
-//   });
-// });
+    expect(routerLink.navigatedTo).toBe('/detail/1');
+  });
+});
